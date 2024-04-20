960554848758cef38704f6b67bccf67cd0c540cc
Nahuel Gómez <nahuel.gomez@ug.uchile.cl>
2024-04-20 19:54:22 -0400

    Updates Back Api managmnet logic

---
+++ django_rest_api/honorarios_api/Connections/google_api.py
@@ -0,0 +1,49 @@
+import requests
+
+def make_google_consult(token, data_json, url):
+    """Makes a post request to the google api with the token and the data_json
+    
+    Arguments:
+        token {str} -- The token to access the google api
+        data_json {json} -- The data to send to the google api
+        url {str} -- The url to send the request
+    
+    Returns:
+        response -- The response from the google api
+    """
+
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    try:
+        response = requests.post(url, headers=headers, json=data_json)
+        if response.status_code == 200:
+            print("Success")
+            return response
+        else:
+            print(f"Error code: {response.status_code}")
+            #print("Message", response.text)
+    except Exception as e:
+        print(f"Error Catch: {str(e)}")
+
+
+
+def make_json(base64, file_type):
+    """Makes a json with the base64 file
+    
+    Arguments:
+        base64 {str} -- The base64 file
+        file_type {str} -- The file type
+    
+    Returns:
+        json -- The json with the base64 file
+    """
+    return {
+    "skipHumanReview": "false",
+    "rawDocument": {
+      "mimeType": file_type,
+      "content": f"{base64}"
+    }
+  }
--- django_rest_api/honorarios_api/views.py
+++ django_rest_api/honorarios_api/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-from rest_framework import viewsets
 from .serializer import File_serializer
-from .models import File_model
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import csv
-from django.http import HttpResponse
 from .mapping import map_clinic_alemana
 import json
+from .Connections.google_api import make_google_consult, make_json
 
 from drf_yasg.utils import swagger_auto_schema
 
@@ -31,81 +27,60 @@ class File_view(APIView):
     def post(self, request):
         serializer = File_serializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            #serializer.save()
+            try:
+                return manage_file(serializer.data)
+            except Exception as e:
+                return Response(str(e), status=status.HTTP_400_BAD_REQUEST)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-import requests
-
-def make_google_consult(token, data_json, url):
-
-    headers = {
-        "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
-    }
-
-    try:
-        response = requests.post(url, headers=headers, json=data_json)
-        if response.status_code == 200:
-            print("Success")
-            return response
-        else:
-            print(f"Error code: {response.status_code}")
-            #print("Message", response.text)
-    except Exception as e:
-        print(f"Error Catch: {str(e)}")
-
-def make_json(base64):
-    return {
-    "skipHumanReview": "false",
-    "rawDocument": {
-      "mimeType": "application/pdf",
-      "content": f"{base64}"
-    }
-  }
-
-
-class UploadPhoto(APIView):
-    def post(self, request, format = None):
-
-        #if 'jpg-file' not in request.FILES:
-        #    return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
         
-        #jpg_file = request.FILES['jpg-file']
-        json_test = make_json(request.data["jpg-file"])
-
-        # Send the jpg file to google
-        google_key, url = get_google_key()
-
-        if DEBUG:
-            print("Good LOG")
-        
-        # Receive the jpg file
-        google_response = make_google_consult(google_key, json_test, url)
-        google_decoded = json.loads(google_response.content)
-
-        if DEBUG:
-            print("Google Response")
-            print(google_decoded)
 
 
-        # Getting the entities keys
-        entities = google_decoded["document"]["entities"]
-        print("asd")
+def manage_file(data):
 
-        if DEBUG:
-            print("Entities keys")
-            for i in entities:
-                print(i['mentionText'])
-                print(i['type'])
+    if DEBUG:
+        print("Received file")
+    
+    b64_file = data["binary"]
+    med = data["med"]
+    clinic = data["clinic"]
+    file_type = data["file_type"]
 
-        # Getting the entities keys values
+    json_test = make_json(b64_file, file_type)
 
+    # Send the jpg file to google
+    google_key, url = get_google_key()
+
+    
+    # Receive the jpg file
+    google_response = make_google_consult(google_key, json_test, url)
+    google_decoded = json.loads(google_response.content)
+
+    if DEBUG:
+        print("Received Google Response")
+
+
+    # Getting the entities keys
+    entities = google_decoded["document"]["entities"]
+
+    if DEBUG:
+        print("Entities keys")
+        for i in entities:
+            print(i['mentionText'])
+            print(i['type'])
+
+    # Getting the entities keys values
+
+    if clinic == "Clinica Alemana":
         response = map_clinic_alemana(entities)
 
+    else:
+        response = "Clinic not supported"
+
+    if DEBUG:
+        print("Data Mapped")
         print(response)
 
-        # return response
-        return Response(response, status=status.HTTP_200_OK)
-
+    # return response
+    return Response(response, status=status.HTTP_200_OK)
