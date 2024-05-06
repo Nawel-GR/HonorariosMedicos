851fe1ac5df995a3f27faed6e3d8238b5d3053a3
Nahuel Gómez <nahuel.gomez@ug.uchile.cl>
2024-05-06 18:02:21 -0400

    Update crud_firebase.py

--- django_rest_api/honorarios_api/utils/crud_firebase.py
+++ django_rest_api/honorarios_api/utils/crud_firebase.py
@@ -13,25 +13,31 @@ def read_document(collection, document_id):
         doc_ref = db.collection(collection).document(document_id)
         document = doc_ref.get()
         if document.exists:
-            return document.to_dict()
+            return 1
         else:
             return 0
     except:
         return 0
-    
+
 # Create a new document in Firestore
-def create_document(collection, document_data):
-    db = firestore.client()
-    doc_ref = db.collection(collection).document()
-    doc_ref.add(document_data)
-    print('Document created with ID:', doc_ref.id)
+def create_document(collection, document_data, document_name):
+    try:
+        db = firestore.client()
+        doc_ref = db.collection(collection).document(document_name)
+        doc_ref.set(document_data)
+    
+    except Exception as e:
+        print(e)
 
 # Update a document in Firestore
 def update_document(collection, document_id, update_data):
-    db = firestore.client()
-    doc_ref = db.collection(collection).document(document_id)
-    doc_ref.update(update_data)
-    print('Document updated successfully!')
+    try:
+        db = firestore.client()
+        doc_ref = db.collection(collection).document(document_id)
+        doc_ref.update(update_data)
+
+    except Exception as e:
+        print(e)
 
 # Delete a document from Firestore
 def delete_document(collection, document_id):
@@ -39,3 +45,29 @@ def delete_document(collection, document_id):
     doc_ref = db.collection(collection).document(document_id)
     doc_ref.delete()
     print('Document deleted successfully!')
+
+def create_consults(worked_day_name, values_l):
+    """ Upload the consults to the firebase
+    Args :
+        worked_day_name (str): The name of the worked day
+        values_l (list) The list of consults
+    Returns:
+        Response: The response of the request
+    """
+    db = firestore.client()
+
+    for i in range(len(values_l)):
+        collection = db.collection("worked_day").document(worked_day_name).collection("dayconsults")
+        collection.document(str(i)).set(values_l[i])
+
+def create_worked_day(doctor_id, clinic_id, document_name):
+    db = firestore.client()
+
+    doctor_ref = db.collection("doctors").document(doctor_id)
+    clinic = db.collection("clinics").document(clinic_id)
+
+    create_document("worked_day", {
+        "doctor": doctor_ref,
+        "clinic": clinic,
+    }, document_name)
+
